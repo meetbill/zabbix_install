86d9fe76feb0844c81226eab1f15edce3c7f03c7
BillWang139967 <wangbin139967@163.com>
2017-02-06 16:05:16 +0800

    update:添加monit守护进程

---
+++ zabbix3.0.6/server/scripts/docker/entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+_file_marker="/var/lib/mysql/.mysql-configured"
+
+if [ ! -f "$_file_marker" ]; then
+    /usr/bin/mysql_install_db
+    /sbin/service mysqld restart
+ 	/usr/bin/mysql_upgrade
+	sleep 10s
+	export MYSQL_PASSWORD="mypassword"
+	echo "mysql root password: $MYSQL_PASSWORD"
+	echo "$MYSQL_PASSWORD" > /mysql-root-pw.txt
+	mysqladmin -uroot password $MYSQL_PASSWORD
+    mysql -uroot -p"$MYSQL_PASSWORD" -e 'CREATE DATABASE `zabbix` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
+    mysql -uroot -p"$MYSQL_PASSWORD" -e "GRANT ALL PRIVILEGES on *.* to zabbix@'localhost' IDENTIFIED BY 'zabbix';"
+    mysql -uroot -p"$MYSQL_PASSWORD" -e "flush privileges"
+    cd /usr/share/doc/zabbix-server-*
+    gunzip create.sql.gz
+    mysql -uzabbix -pzabbix zabbix < create.sql
+	/sbin/service mysqld stop
+	touch "$_file_marker"
+fi
+
+_cmd="/usr/bin/monit -Ic /etc/monitrc"
+_shell="/bin/bash"
+
+case "$1" in
+	start)
+        echo "Running Monit... "
+        exec /usr/bin/monit -Ic /etc/monitrc
+        $_cmd monitor all
+		;;
+	stop)
+		$_cmd stop all
+    RETVAL=$?
+		;;
+	restart)
+		$_cmd restart all
+    RETVAL=$?
+		;;
+  shell)
+    $_shell
+    RETVAL=$?
+		;;
+	status)
+		$_cmd status all
+    RETVAL=$?
+		;;
+  summary)
+		$_cmd summary
+    RETVAL=$?
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|restart|shell|status|summary}"
+		RETVAL=1
+esac
+
--- zabbix3.0.6/server/zabbix_server.sh
+++ zabbix3.0.6/server/zabbix_server.sh
@@ -13,10 +13,6 @@ info_echo(){
     echo -e "\033[42;37m[Info]: $1 \033[0m"
 }
   
-warn_echo(){
-    echo -e "\033[43;37m[Warning]: $1 \033[0m"
-}
-  
 check_exit(){
     if [ $? -ne 0 ]; then
         err_echo "$1"
@@ -51,179 +47,24 @@ setenforce 0
 # yum repo
 ##############################################
 info_echo "配置yum源......"
-if [ ! -f LNMP+zabbix.repo ]; then
-cat> /etc/yum.repos.d/LNMP+zabbix.repo <<'EOF'
-[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/centos/6/$basearch/
-gpgcheck=0
-enabled=1
-  
-[webtatic]
-name=Webtatic Repository EL6 - $basearch
-#baseurl=http://repo.webtatic.com/yum/el6/$basearch/
-mirrorlist=http://mirror.webtatic.com/yum/el6/$basearch/mirrorlist
-failovermethod=priority
-enabled=0
-gpgcheck=0
- 
-[epel] 
-name=Extra Packages for Enterprise Linux 6 - $basearch 
-baseurl=http://mirrors.aliyun.com/epel/6/$basearch 
-http://mirrors.aliyuncs.com/epel/6/$basearch 
-#mirrorlist=https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch 
-failovermethod=priority 
-enabled=1 
-gpgcheck=0
- 
-[remi]
-name=Les RPM de remi pour Enterprise Linux 6 - $basearch
-#baseurl=http://rpms.famillecollet.com/enterprise/6/remi/$basearch/
-mirrorlist=http://rpms.famillecollet.com/enterprise/6/remi/mirror
-enabled=1
-gpgcheck=0
- 
-#[zabbix]
-#name=Zabbix Official Repository-$basearch
-#baseurl=http://repo.zabbix.com/zabbix/3.0/rhel/6/$basearch/
-#enabled=1
-#gpgcheck=0
-  
-#[zabbix-non-supported]
-#name=Zabbix Official Repository non-supported-$basearch
-#baseurl=http://repo.zabbix.com/non-supported/rhel/6/$basearch/
-#enabled=1
-#gpgcheck=0
- 
-EOF
- 
-fi
+cp env/lnmp.repo /etc/yum.repos.d/
 ##############################################
 # Install nginx+Mysql+PHP+zabbix
 ##############################################
 info_echo "Install nginx+Mysql+PHP+zabbix......"
- 
-yum -y install nginx php php-fpm php-cli php-common php-gd php-mbstring php-mcrypt php-mysql php-pdo php-devel php-imagick php-xmlrpc php-xml php-bcmath php-dba php-enchant php-yaf  mysql mysql-server
+yum -y install nginx php php-fpm php-cli php-common php-gd php-mbstring php-mcrypt php-mysql php-pdo php-devel php-imagick php-xmlrpc php-xml php-bcmath php-dba php-enchant php-yaf  mysql mysql-server monit
 check_exit "Failed to install Nginx/PHP/Mysql"
 yum -y install ./packages/*.rpm
 #yum -y install zabbix zabbix-get zabbix-agent zabbix-server-mysql zabbix-web-mysql zabbix-server
-check_exit "Failed to install Zabbix"
+#check_exit "Failed to install Zabbix"
   
 #########################################
 # Nginx 
 #########################################
 info_echo "Nginx 配置文件更新 ...."
- 
-if [ -f /etc/nginx/nginx.conf ]; then
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-cat> /etc/nginx/nginx.conf <<'EOF'
-user deploy;
-worker_processes 2;
-pid /var/run/nginx.pid;
-worker_rlimit_nofile 65535;
-events {
-    worker_connections  65535;
-    use epoll;
-}
-http {
-   ##
-    # Basic Settings
-   ##
-     sendfile on;
-     tcp_nopush on;
-     tcp_nodelay on;
-       
-     keepalive_timeout 65;
-     types_hash_max_size 2048;
-     server_tokens off;
-     
-     client_header_buffer_size 4k;
-     open_file_cache max=65535 inactive=60s;
-     open_file_cache_valid 80s;
-     open_file_cache_min_uses 1;
-     server_names_hash_bucket_size 64;
-     server_name_in_redirect off;
-     include /etc/nginx/mime.types;
-     default_type application/octet-stream;
-   ##
-    # Logging Settings
-   ##
-     access_log /var/log/nginx/access.log;
-     error_log /var/log/nginx/error.log;
-    
-  ##
-   # Gzip Settings
-  ##
-     gzip on;
-     gzip_disable "msie6";
-     gzip_min_length 1k;
-     gzip_buffers 4 16k;
-     gzip_comp_level 2;
-     gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
-   ##
-   # nginx-naxsi config
-   ##
-      # Uncomment it if you installed nginx-naxsi
-      ##
-      #include /etc/nginx/naxsi_core.rules;
-    ##
-    # nginx-passenger config
-    ##
-    # Uncomment it if you installed nginx-passenger
-    ##
-      
-    #passenger_root /usr;
-    #passenger_ruby /usr/bin/ruby;
-    ##
-    # Virtual Host Configs
-    ##
-        log_format  main  '$server_name $remote_addr - $remote_user [$time_local] "$request" '
-                        '$status $body_bytes_sent "$http_referer" '
-                        '"$http_user_agent" "$http_x_forwarded_for" '
-                        '$ssl_protocol $ssl_cipher $upstream_addr $request_time $upstream_response_time';
-    include /etc/nginx/conf.d/*.conf;
-    include /etc/nginx/sites-enabled/*.conf;
-}
- 
-EOF
- 
-fi
- 
-sed -i "/worker_processes/cworker_processes $( grep "processor" /proc/cpuinfo| wc -l );" /etc/nginx/nginx.conf
- 
-info_echo "zabbix config add"
-cat> /etc/nginx/conf.d/zabbix.conf <<'EOF'
-server{
-   listen       80;
-   server_name  _;
-  
-   index index.php;
-   root /data/web/zabbix;
-  
-   location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
-       expires 30d;
-   }
-  
-   location ~* \.php$ {
-       fastcgi_pass   127.0.0.1:9000;
-       fastcgi_index  index.php;
-       fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
-       include        fastcgi_params;
-   }
-}
-EOF
-mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+cp ./conf/nginx/nginx.conf /etc/nginx/
 /etc/init.d/nginx restart
   
-  
-#########################################
-# Zabbix 
-#########################################
-#cp ./zabbix.tar.gz /tmp
-#if [ ! -f /tmp/zabbix.tar.gz ]; then
-#   cd /tmp && wget -O zabbix.tar.gz 'http://sourceforge.net/projects/zabbix/files/latest/download?source=files'
-#fi
- 
 mkdir -p /data/web/zabbix
 tar -zxf zabbix.tar.gz
 ZABBIX_DIR=`ls |grep zabbix-`
@@ -234,68 +75,30 @@ else
     exit 1
 fi
 chown -R deploy.deploy /data/web/zabbix
-cp ./conf/zabbix.conf.php /data/web/zabbix/conf/
+cp conf/zabbix/zabbix.conf.php /data/web/zabbix/conf/
+cp conf/zabbix/zabbix_server.conf /etc/zabbix/
   
   
 ##############################################
 # Database
 ##############################################
-info_echo "Mysql配置文件更新..."
-sed -i '/^socket/i\port            = 3306' /etc/my.cnf
-sed -i '/^socket/a\skip-external-locking\nkey_buffer_size = 256M\nmax_allowed_packet = 1M\ntable_open_cache = 256\nsort_buffer_size = 1M\nread_buffer_size = 1M\nread_rnd_buffer_size = 4M\nmyisam_sort_buffer_size = 64M\nthread_cache_size = 8\nquery_cache_size= 16M\nthread_concurrency = 4\ncharacter-set-server=utf8\ninnodb_file_per_table=1' /etc/my.cnf
- 
-info_echo "Restart mysql ..."
-/etc/init.d/mysqld start
- 
-info_echo "Create Databases..." 
-mysql -e 'CREATE DATABASE `zabbix` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;'
-mysql -e "GRANT ALL PRIVILEGES on *.* to zabbix@'localhost' IDENTIFIED BY 'zabbix';"
-mysql -e "flush privileges"
- 
-info_echo "配置zabbix的数据库项"
-sed -i '/DBPassword=/a\DBPassword=zabbix' /etc/zabbix/zabbix_server.conf
- 
-info_echo "importing sql"
-cd /usr/share/doc/zabbix-server-*
-gunzip create.sql.gz
-mysql -uzabbix -pzabbix zabbix < create.sql
-  
+cp ./conf/mysql/my.cnf /etc/
+
 #########################################
 # PHP-FPM
 #########################################
  
-info_echo "更新/etc/php.ini,www.conf ..."
-sed -i '/^;default_charset/a\default_charset = "UTF-8"' /etc/php.ini
-sed -i '/^expose_php/cexpose_php = Off' /etc/php.ini
-sed -i '/^max_execution_time/cmax_execution_time = 300' /etc/php.ini
-sed -i '/^max_input_time/cmax_input_time = 300' /etc/php.ini
-sed -i '/^memory_limit/cmemory_limit = 256M'  /etc/php.ini
-sed -i '/^post_max_size/cpost_max_size = 32M' /etc/php.ini
-sed -i '/^upload_max_filesize/cupload_max_filesize = 300M' /etc/php.ini
-sed -i '/^max_file_uploads/cmax_file_uploads = 30' /etc/php.ini
-sed -i '/^;date.timezone/cdate.timezone = "PRC"' /etc/php.ini
-sed -i 's/apache/deploy/g' /etc/php-fpm.d/www.conf 
+cp conf/php/php.ini /etc/
+cp conf/php/www.conf /etc/php-fpm.d/
 chown deploy.deploy -R /var/lib/php
- 
-info_echo "Checking php-fpm configuration file..."
-/etc/init.d/php-fpm configtest
-check_exit "PHP-FPM configuration syntax error"
-  
-info_echo "Restart PHP-FPM ..."
-/etc/init.d/php-fpm restart
-  
-info_echo "Restart Zabbix Server ..."
-/etc/init.d/zabbix-server restart
-  
-info_echo "Restart Zabbix Agent ..."
-/etc/init.d/zabbix-agent restart
-  
-#########################################
-# 开机启动项
-#########################################
-chkconfig nginx on
-chkconfig php-fpm on
-chkconfig mysqld on
-chkconfig zabbix-agent on
-chkconfig --add zabbix-server
-chkconfig zabbix-server on
+#/etc/init.d/php-fpm configtest
+
+#######monit配置
+cp scripts/install/monitrc /etc/monitrc
+chmod 600 /etc/monitrc
+cp scripts/install/entrypoint.sh /bin/zabbix-monit
+chmod +x /bin/zabbix-monit
+ln -s /bin/zabbix-monit /etc/init.d/zabbix-monit 
+chkconfig --add zabbix-monit
+chkconfig zabbix-monit on
+/etc/init.d/zabbix-monit start
